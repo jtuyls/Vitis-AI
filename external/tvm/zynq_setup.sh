9525430518012336b2a8f79a60f803c73ac20b5e
Jorn Tuyls <jtuyls@users.noreply.github.com>
2021-04-14 11:37:45 +0100

    Update zynq_setup.sh

--- external/tvm/zynq_setup.sh
+++ external/tvm/zynq_setup.sh
@@ -24,10 +24,11 @@ export TVM_HOME="${TVM_VAI_HOME}"/tvm
 export PYXIR_HOME="${TVM_VAI_HOME}"/pyxir
 
 # DOWNLOADS
-git clone --recursive --branch v0.1.3 https://github.com/Xilinx/pyxir.git "${PYXIR_HOME}"
-git clone --recursive https://github.com/apache/incubator-tvm.git "${TVM_HOME}"
+git clone --recursive --branch v0.1.6 https://github.com/Xilinx/pyxir.git "${PYXIR_HOME}"
+git clone --recursive https://github.com/apache/tvm.git "${TVM_HOME}"
 
- 
+apt-get update && apt-get install libhdf5-dev
+
 # DOWNLOAD REQUIRED PYTHON PACKAGES
 pip3 install cffi cython progressbar h5py==2.8.0
  
@@ -39,7 +40,7 @@ sudo python3 setup.py install --use_vai_rt_dpuczdx8g
 cd "${TVM_HOME}"
 mkdir "${TVM_HOME}"/build
 cp "${TVM_HOME}"/cmake/config.cmake "${TVM_HOME}"/build/
-cd "${TVM_HOME}"/build && echo set\(USE_VITIS_AI ON\) >> config.cmake && cmake .. && make tvm_runtime -j$(nproc)
+cd "${TVM_HOME}"/build && echo set\(USE_VITIS_AI ON\) >> config.cmake && echo set\(USE_LLVM OFF\) >> config.cmake && cmake .. && make tvm_runtime -j$(nproc)
 cd "${TVM_HOME}"/python && pip3 install -e .
 
 
